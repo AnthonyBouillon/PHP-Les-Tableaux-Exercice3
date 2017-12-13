0666b19fd859ec02b4f2b8251e77b12cddf07cf2
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 13:55:22 +0100

    init commit from bash for PHP-Les-Tableaux-Exercice3

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les tableaux Exercice 3</title>
+  </head>
+  <body>
+    <?php
+      $month = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+    // index 5 = chiffre 5 et non le 5ème éléments;
+      echo $month[5];
+     ?>
+  </body>
+</html>
